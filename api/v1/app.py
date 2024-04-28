34840552e9be7351d8b3b549126166cabb0b8add
mahakamal-e <mahakamal09@gmail.com>
2024-04-28 10:22:55 -0700

    edit

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ This app.py file sets up a Flask application
 """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from os import getenv
 from api.v1.views import app_views
@@ -16,6 +16,13 @@ def terminate(exc):
     """ Method used to close AQLAlchemy session"""
     storage.close()
 
+
+@app.errorhandler(404)
+def error_not_found(error):
+    """handle not found """
+    response = jsonify({"error": "Not found"})
+    response.status_code = 404
+    return response
 if __name__ == "__main__":
     app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
             port=getenv('HBNB_API_PORT', 5000),
