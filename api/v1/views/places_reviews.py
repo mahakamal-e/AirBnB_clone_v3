195667acc709709d8cbdf8c94dc14570aebe1022
mahakamal-e <mahakamal09@gmail.com>
2024-04-30 15:44:48 -0700

    deit5

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -54,13 +54,13 @@ def create_review(place_id):
         return jsonify({"error": "Not a JSON"}), 400
     if 'user_id' not in data.keys():
         return jsonify({"error": "Missing user_id"}), 400
-    if 'text' not in data.keys():
-        return jsonify({"error": "Missing text"}), 400
 
     get_users = storage.all(User)
     user = storage.get('User', data['user_id'])
     if user is None:
         abort(404)
+    if 'text' not in data.keys():
+        return jsonify({"error": "Missing text"}), 400
 
     data['place_id'] = place_id
     new_review = Review(**data)
