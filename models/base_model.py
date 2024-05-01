ba36de58fed171ff7d6a0bb915381ddbda6b14f1
mahakamal-e <mahakamal09@gmail.com>
2024-04-30 19:59:10 -0700

    ee

--- models/base_model.py
+++ models/base_model.py
@@ -69,7 +69,7 @@ class BaseModel:
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
 
-        if getenv("HBNB_TYPE_STORAGE") == "db" and "password" in new_dict:
+        if getenv("HBNB_TYPE_STORAGE") == "db" and new_dict.get("password"):
             del new_dict["password"]
         return new_dict
 
